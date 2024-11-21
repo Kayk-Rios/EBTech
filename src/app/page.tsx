492ae89095a1b1dba8d09107408e8c2128229c7c
Kayk-Rios <manjac688@gmail.com>
2024-11-20 21:46:50 -0300

    style:sectionWidth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,40 @@ export default function Home() {
   return (
     <div>
       <NavBar/>
-      <section className="pl-10 flex  justify-start items-center bg-white w-full h-[800px]">
+      <section className="pl-6 flex  justify-start items-center bg-white h-[800px]">
         <motion.div
           initial={{ opacity: 0, x: -100 }} 
           animate={{ opacity: 1, y: 0 }} 
           whileInView={{opacity:1, x: 0}}
           transition={{ duration: 0.5 }}
-          style={{  padding: "20px", borderRadius: "8px"}}
           exit={{opacity: 0, x:-50}}
         >
-          <p className="text-black w-[300px]">Por um ambiente corporativo mais saudável e justo.Nosso programa oferece treinamentos em tecnologias essenciais às empresas que investem em qualidade de vida para seus funcionários por meio de convênios de saúde.</p>
+          <p className="text-black  max-w-[200px] sm:max-w-[300px] md:max-w-[300px]">Por um ambiente corporativo mais saudável e justo.Nosso programa oferece treinamentos em tecnologias essenciais às empresas que investem em qualidade de vida para seus funcionários por meio de convênios de saúde.</p>
         </motion.div>
 
       </section>
-      <section className="pr-10 flex  justify-end items-center  sectTion2 w-full h-[800px]">
+      <section className="pr-6 flex  justify-end items-center  sectTion2 w-full h-[800px]">
         <motion.div
          initial={{ opacity: 0, x: -100 }} 
          animate={{ opacity: 1, y: 0 }} 
          whileInView={{opacity:1, x: 0}}
          transition={{ duration: 0.5 }}
-         style={{  padding: "20px", borderRadius: "8px"}}
          exit={{opacity: 0, x:-50}}
         >
-        <p className="text-white w-[300px]"> Mais do que benefícios físicos, promovemos saúde mental com um propósito transformador.
+        <p className="text-white  max-w-[200px] sm:max-w-[300px] md:max-w-[300px]"> Mais do que benefícios físicos, promovemos saúde mental com um propósito transformador.
         Imagine seus colaboradores passando por sessões terapêuticas em realidade virtual, apoiados por psicólogos especializados, que vão além da terapia tradicional. </p>
         </motion.div>
 
       </section>
-      <section className="pl-10 flex  justify-start items-center  bg-white w-full h-[800px]">
+      <section className="pl-6 flex  justify-start items-center  bg-white w-full h-[800px]">
         <motion.div
           initial={{ opacity: 0, x: 100 }} 
           animate={{ opacity: 1, y: 0 }} 
           whileInView={{opacity:1, x: 1}}
           transition={{ duration: 0.5 }}
-          style={{  padding: "20px", borderRadius: "8px"}}
           exit={{opacity: 0, x: -100}}
         >
-        <p className="text-black w-[300px]"> Analisamos perfis comportamentais para identificar atitudes que podem contribuir para a desigualdade de gênero no ambiente corporativo. Com esses insights, ajudamos sua empresa a cumprir a nova lei de projetos de igualdade de gênero, prevenindo penalidades e promovendo um ambiente inclusivo e harmonioso</p>
+        <p className="text-black  max-w-[200px] sm:max-w-[300px] md:max-w-[300px]"> Analisamos perfis comportamentais para identificar atitudes que podem contribuir para a desigualdade de gênero no ambiente corporativo. Com esses insights, ajudamos sua empresa a cumprir a nova lei de projetos de igualdade de gênero, prevenindo penalidades e promovendo um ambiente inclusivo e harmonioso</p>
         </motion.div>
 
       </section>
